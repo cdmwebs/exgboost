f83f088dbb9ba0ba337470991491567cacfd2334
Chris Moore <chris@cdmwebs.com>
2025-09-09 13:35:40 -0400

    Update exterval

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule EXGBoost.MixProject do
       {:jason, "~> 1.3"},
       {:ex_doc, "~> 0.31.0", only: :docs},
       {:cc_precompiler, "~> 0.1.0", runtime: false},
-      {:exterval, "0.1.0"},
+      {:exterval, "0.2.0"},
       {:ex_json_schema, "~> 0.11.0"},
       {:httpoison, "~> 2.0", runtime: false},
       {:vega_lite, "~> 0.1"},
